9c55a94ef09e8b29cf4f9f58c6d84f5c6525b8e5
kunmeng <49559439@qq.com>
2016-10-01 02:04:50 +0800

    Initial commit

---
+++ hello-word/src/com/dina/test/HelloWord.java
@@ -0,0 +1,10 @@
+package com.dina.test;
+
+/**
+ * Created by mk on 2016/10/1.
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("Hello word");
+    }
+}
